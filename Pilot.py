ab4b5eb2c6a0adaed6b572c5ce97ccc82fd745b2
Kouakou Samuel Oussou <kouakou.oussou@telecom-paris.fr>
2023-06-26 08:49:24 +0000

    Fichier de pilotage du drone

---
+++ Pilot.py
@@ -0,0 +1,43 @@
+import socket,time, cv2
+ 
+
+tello_ip = '192.168.10.1'
+tello_port = 8889
+tello_address = (tello_ip, tello_port)
+host ='192.168.1.15'
+
+#mypc_address = (host, port)
+
+local_ip=''
+socket = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+socket.bind ((local_ip,8889))
+socket.sendto ('command'.encode (' utf-8 '), tello_address)
+socket.sendto ('streamon'.encode (' utf-8 '), tello_address)
+print ("Start streaming")
+
+capture = cv2.VideoCapture ('udp:/0.0.0.0:11111',cv2.CAP_FFMPEG)
+if not capture.isOpened():
+    capture.open('udp:/0.0.0.0:11111')
+
+def sendToDrone(command):
+    labels = {'neutre' : '', 'decollage' : 'takeoff', 'droite' : 'right 50', 'gauche' : 'left 50', 'atterir' : 'land', 'reculer' : 'back 50', 'rapprocher' : 'forward 50', 'flip' : 'flip r', 'gear second' : 'speed 20','fortnite' : 'flip b'}
+    socket.sendto(labels[command].encode ('utf-8'), tello_address)
+    
+while True:
+    #Récupérer la sortie vidéo
+    ret, frame =capture.read()
+    #print(ret)
+    if(ret):
+        cv2.imshow('frame', frame)
+        
+    if cv2.waitKey (1)&0xFF == ord ('q'):
+        break
+
+    command = "decollage"
+    sendToDrone(command)
+
+    
+
+capture.release ()
+cv2.destroyAllWindows ()
+socket.sendto ('streamoff'.encode (' utf-8 '), tello_address)
